d98f14538ea3117d28712775b8ae992f50beea21
Maksim <maksimum232@gmail.com>
2022-08-06 17:28:24 +0200

    Added methods for getting books

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 
@@ -20,15 +21,57 @@ type Author struct {
 	Lastname  string `json:"lastname"`
 }
 
+// Collection of books var
+var books []Book
+
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
+}
+
+func getBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	id := mux.Vars(r)["id"]
+
+	for _, book := range books {
+		if book.ID == id {
+			json.NewEncoder(w).Encode(book)
+			return
+		}
+	}
+
+	json.NewEncoder(w).Encode(Book{})
+}
+
+// func createBook(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type", "application/json")
+// 	var book Book
+
+// }
+
+func updateBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func main() {
 	r := mux.NewRouter()
 
+	books = append(books, Book{ID: "1", Isbn: "448743", Title: "Book One",
+		Author: Author{Firstname: "John", Lastname: "Doe"}})
+
+	books = append(books, Book{ID: "2", Isbn: "847564", Title: "Book Two",
+		Author: Author{Firstname: "Steve", Lastname: "Smith"}})
+
 	// Route Handlers
-	r.HandleFunc("api/books", getBooks).Methods("GET")
-	r.HandleFunc("api/books/{id}", getBook).Methods("GET")
-	r.HandleFunc("api/books", createBook).Methods("POST")
-	r.HandleFunc("api/books/{id}", updateBook).Methods("PUT")
-	r.HandleFunc("api/books/{id}", deleteBook).Methods("DELETE")
+	r.HandleFunc("/books", getBooks).Methods("GET")
+	r.HandleFunc("/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/books", createBook).Methods("POST")
+	r.HandleFunc("/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/books/{id}", deleteBook).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
